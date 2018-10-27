0a0d6b989531ee96a8a2d0c38d2f66a989ead630
Joshua Bearden <josh.e.bearden@gmail.com>
2018-10-27 17:02:46 -0500

    added token.h

--- main.c
+++ main.c
@@ -52,8 +52,6 @@ int main(int argc, char * argv[])
         exit(1);
     }
 
-    
-
     // Close the file unless the file is stdin
     if (fp != stdin)
     {
---
+++ token.h
@@ -0,0 +1,24 @@
+//
+// Created by fof_z on 10/27/2018.
+//
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef enum
+{
+    beginTk, endTk, iterTk, voidTk, varTk, returnTk, readTk, printTk,
+    programTk, condTk, thenTk, letTk, equalTk, greaterTk, lessTk, colonTk,
+    plusTk, minusTk, starTk, slashTk, percentTk, dotTk, openparenTk,
+    closeparenTk, commaTk, openbraceTk, closebraceTk, semicolonTk,
+    openbracketTk, closebracketTk, identifierTk, integerTk, eofTk
+} tokens;
+
+typedef struct token_t
+{
+    tokens tokenID;
+    char[8] tokenInstance;
+    int line;
+} token;
+
+#endif //TOKEN_H
