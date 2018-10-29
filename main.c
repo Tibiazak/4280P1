f55856ff198063c55aecea5720132a8a1f778649
Joshua Bearden <josh.e.bearden@gmail.com>
2018-10-28 19:58:09 -0500

    yet another bugfix

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ int main(int argc, char * argv[])
     while(!done)
     {
         newtoken = scan(fp);
-        if(newtoken.line = EOF)
+        if(newtoken.line == EOF)
         {
             done = true;
         }
